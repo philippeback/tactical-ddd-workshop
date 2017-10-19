a8e87f460329c4807e62271a69aaae933a12b5f4
Philippe Back <phil@highoctane.be>
2017-10-19 15:38:53 +0200

    Added logging to see more clearly how the event is handled.

--- src/MeetupOrganizing/Domain/Model/Meetup/MeetupScheduledHandler.php
+++ src/MeetupOrganizing/Domain/Model/Meetup/MeetupScheduledHandler.php
@@ -13,13 +13,16 @@ final class MeetupScheduledHandler
     public function __invoke($event)
     {
         dump('Handling Meetup Scheduled');
+        dump('Event is:');
         dump($event);
 
+        // This should be done in an application service
         $rsvp = Rsvp::yes(
             RsvpId::fromString((string)Uuid::uuid4()),
             $event->meetupId(),
             $event->organizerId()
         );
+        dump('Rsvp produced is:');
         dump($rsvp);
         dump('Handling Meetup Performed');
 
